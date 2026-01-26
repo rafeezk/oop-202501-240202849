c168a746d6a9517922a5d21a10ea241f32e0025d
raavfy azzaqee <142195393+rafeezk@users.noreply.github.com>
2026-01-26 20:12:47 +0700

    Add files via upload

---
+++ praktikum/week9-exception-handling/src/main/java/com/upb/agripos/ProductService.java
@@ -0,0 +1,17 @@
+package com.upb.agripos;
+
+public class ProductService {
+
+    private static ProductService instance;
+
+    private ProductService() {}
+
+    public static ProductService getInstance() {
+
+        if (instance == null) {
+            instance = new ProductService();
+        }
+
+        return instance;
+    }
+}
